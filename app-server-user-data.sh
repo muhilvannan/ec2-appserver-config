80b2f9a3d47c2d0e42f0c5b98132a57e07383e8e
Muhil <voldemort.muhil@gmail.com>
2015-05-22 09:24:38 +0100

    added ssl folder structure configuration

--- app-server-user-data.sh
+++ app-server-user-data.sh
@@ -42,3 +42,15 @@ mkdir /var/log/backup-log/daily
 mkdir /var/log/backup-log/weekly
 mkdir /var/log/backup-log/monthly
 echo 'backup folder structure configured'
+
+mkdir /etc/httpd/ssl
+mkdir /etc/httpd/ssl/bundles
+mkdir /etc/httpd/ssl/certs
+mkdir /etc/httpd/ssl/keys
+mkdir /etc/httpd/ssl/csr
+chmod 644 /etc/httpd/ssl/bundles
+chmod 644 /etc/httpd/ssl/certs
+chmod 600 /etc/httpd/ssl/keys
+chmod 644 /etc/httpd/ssl/csr
+
+echo 'SSL folders configured ....'
